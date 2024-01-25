7da47647a841c4544ec4768fd8daa60c195a4716
Athul <athulkrishnatv702@gmail.com>
2024-01-25 16:14:57 +0530

    youtube done

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,12 +4,12 @@ const Footer = () => {
   return (
     <div class="footer">
         <p>&copy; 2024 Your Website Name. All Rights Reserved.</p>
-        <div class="footer-links">
+        {/* <div class="footer-links">
             <a>Home</a>
             <a >Terms of Use</a>
             <a >Privacy Policy</a>
             <a >Contact Us</a>
-        </div>
+        </div> */}
        
     </div>
   )
